475c69ac3cda7feba691d3e9ad069430c787fc8e
DnTo <danto@hotmail.com>
2023-10-07 16:55:39 -0500

    feat(back): WIP endpoint login

---
+++ backend/Common/Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿namespace S11.Common.Interfaces
+{
+    public interface IAuthService
+    {
+        object Login(string username, string password);
+        void RestorePassword(string username);
+
+    }
+}
---
+++ backend/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using S11.Common.Dtos.Auth;
+using S11.Common.Interfaces;
+
+namespace S11.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService )
+        {
+            _authService = authService;
+        }
+
+        //TODO definir que devuelve + un error de validacion
+        [HttpPost]
+        public ActionResult<object> Login(LoginPostDto login)
+        {
+            //userName
+            //rol
+            //token
+            return _authService.Login(login.UserName, login.Password);
+        }
+
+        [HttpPost]
+        public ActionResult RestorePassword(string userName)
+        {
+            _authService.RestorePassword(userName);
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult ChangePassword(string userName,string password, string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
