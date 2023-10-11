e1bd02aad5beb2e4b8d105a391b5b9aeff46cb1a
Daniel Torres <danto@hotmail.com>
2023-10-11 15:39:37 -0500

    feat(back): Login endpoint (#8)
    * feat(back): Connect with TokenService
    
    * feat(back): Connect with TokenService

---
+++ backend/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using S11.Common.Dtos.Auth;
+using S11.Common.Interfaces;
+
+namespace S11.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        //TODO definir que devuelve + un error de validacion
+        [HttpPost(nameof(Login))]
+        public async Task<ActionResult<LoginResponseDto>> Login(LoginPostDto login)
+        {
+            //userName
+            //rol
+            await _authService.CreateTestUsers();
+            var res = await _authService.Login(login.UserName, login.Password);
+            return res;
+            //token
+        }
+
+        [HttpPost(nameof(RestorePassword))]
+        public ActionResult RestorePassword(string userName)
+        {
+            _authService.RestorePassword(userName);
+            return Ok();
+        }
+
+        [Obsolete(message:"not implemented yet")]
+        [HttpPost(nameof(ChangePassword))]
+        public ActionResult ChangePassword(string userName, string password, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet(nameof(TestAuthorize))]
+        [Authorize]
+        public IActionResult TestAuthorize()
+        { return Ok(); }
+    }
+}
