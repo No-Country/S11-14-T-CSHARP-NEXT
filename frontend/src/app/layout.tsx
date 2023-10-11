e079e92a0bf71e8786caf8b86111468a223034a7
Santiago Rodriguez <05rmsantiago@gmail.com>
2023-10-11 19:10:09 -0300

    chore(Frontend): :wrench: add ui properties in tailwind

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -14,6 +14,10 @@ interface Props {
 const RootLayout = ({children}: Props) => {
   return (
       <html lang="es">
+        <head>
+          <link href="https://fonts.cdnfonts.com/css/sf-pro-display" rel="stylesheet" ></link>
+          <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet"></link>
+        </head>
         <body className='h-screen'>
           <NextUIProvider>
             {children}
