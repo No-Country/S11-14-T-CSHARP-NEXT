654f3313c93fe714e724e860ebc95aa68db09f50
Santiago Rodriguez <84601454+mastoreto@users.noreply.github.com>
2023-10-12 13:55:54 -0300

    Front/ns 52 install dependencies (#11)
    * chore(Frontend): :wrench: add ui properties in tailwind
    
    * chore(Frontend): :sparkles: add next auth provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@styles/globals.css'
 import type { Metadata } from 'next'
 import NextUIProvider from '@libs/NextUIProvider';
+import AuthProvider from '@libs/Auth/Provider/indext';
 
 export const metadata: Metadata = {
   title: 'HotelWiz',
@@ -19,9 +20,11 @@ const RootLayout = ({children}: Props) => {
           <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet"></link>
         </head>
         <body className='h-screen'>
-          <NextUIProvider>
-            {children}
-          </NextUIProvider>
+          <AuthProvider>
+            <NextUIProvider>
+              {children}
+            </NextUIProvider>
+          </AuthProvider>
         </body>
       </html>
   )
