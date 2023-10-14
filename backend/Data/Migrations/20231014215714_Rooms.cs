9ae816900b05743fed85ba4a0150ce06323899b3
mldamico <matias.lioneldamico@gmail.com>
2023-10-14 18:58:36 -0300

    Feat: Room migration

---
+++ backend/Data/Migrations/20231014215714_Rooms.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace S11.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Rooms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    RoomId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.RoomId);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEMB6P16/0mRmr3+rwnNwgOXpY1VxF5XuTyN4MhHy42cHq8WqDU0w8ye48YL+L9tpzw==");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAELvKVG802ScMNz0pLDTsqnuSsoGVCZkhW+B9+Fl9eFOi0B/EbMDBa1RsRTAdxxId/Q==");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEBWGRDI3urVnMGmc28UL4KdhbCHILkzkr6dU/hn0Nbpplh1HmLZv9xMcr57FU4aBTw==");
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "RoomId", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, "A single room to sleep comfortable", "Single" },
+                    { 2, "Couple room", "Double" },
+                    { 3, "Triple room", "Triple" },
+                    { 4, "Quad room", "Quad" },
+                    { 5, "Queen room", "Queen" },
+                    { 6, "King room", "King" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rooms");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEM+BGCPmgNLjnHrdZ6Ve1EO/yM3QEcmEikB5nY05neqMATaIFlFVD5bp361aK9mUAg==");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEG7XbbgbV6EqR+lAEZ4Lr28UzMD6Kfo6SBarl7kYY9M2CYyyQPZXBo9/RiZ1+Bg7Vw==");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEFHVZpfpeZa6MvWNUk3LAHxg7Ne79aVecLCZa/sCben+c9cOHUnFOXM5xRnujOvAbQ==");
+        }
+    }
+}
