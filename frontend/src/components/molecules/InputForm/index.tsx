991b9b755d7b8c178374111936bdb485bf0d07d3
Noé Alonso Rabadán <nalonsor@outlook.com>
2023-10-16 09:49:23 -0600

    feat(Frontend): :art: Login && Validations library (#21)
    The frontend login and the custom validations labrary has been added

--- frontend/src/components/molecules/InputForm/index.tsx
+++ frontend/src/components/molecules/InputForm/index.tsx
@@ -3,41 +3,34 @@ import Label from '@components/atoms/Label';
 import React from 'react';
 
 interface InputFormProps {
+  handleChange: Function;
   labelText: string;
   inputType: string;
   inputName: string;
   inputPlaceholder?: string;
   value: string;
-  setValues: React.Dispatch<React.SetStateAction<{ username: string; password: string }>>;
 }
 
 const InputForm: React.FC<InputFormProps> = ({
+  handleChange,
   labelText,
   inputName,
   inputType,
   inputPlaceholder,
   value,
-  setValues,
 }) => {
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setValues((prevValues) => ({
-      ...prevValues,
-      [name]: value,
-    }));
-    // setIsButtonDisabled(values.username === '' || values.password === '');
-  };
+
 
   return (
     <div className='flex flex-col'>
       <Label htmlFor='username' text={labelText} className='text-left text-[#232130] opacity-50' />
       <Input
+        handleChange={handleChange}
         type={inputType}
         name={inputName}
         value={value}
-        onChange={handleChange}
         placeholder={inputPlaceholder}
-        className='text-[#232130] opacity-50 px-4 py-2 rounded-[5px] bg-white border border-[#D9D9D9]'
+        className='text-gray-700 px-4 py-2 rounded-[5px] bg-white border border-2 border-gray-300'
       />
     </div>
   );
