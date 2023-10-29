28d896c4e054b97927d5fbabd159f24b829a26a1
Daniel Torres <danto@hotmail.com>
2023-10-29 09:06:14 -0500

    fix(resume): optimize response (#39)
    removed unused data, and call async resumes

--- Services/RoomService.cs
+++ Services/RoomService.cs
@@ -94,46 +94,56 @@ public class RoomService
     }
 
     //TODO too slow
-    public RoomResumeDto GetResume()
+    public async Task<RoomResumeDto> GetResume()
     {
-        var data = _contexto.Rooms.AsQueryable();
+        var data = _contexto.Rooms;
+
+        var d = new RoomResumeDto
+        {
+            TotalTaken = await data.CountAsync(x => x.Status == RoomStatus.Reservada),
+            TotalFree = await data.CountAsync(x => x.Status == RoomStatus.Libre),
+            TotalRooms = await data.CountAsync(),
+            TotalMaintenance = await data.CountAsync(x => x.Status == RoomStatus.Mantenimiento)
+        };
 
         var dto = new RoomResumeDto()
         {
-            Data = data.Select(x => new RoomDto()
-            {
-                RoomId = x.RoomId,
-                RoomNumber = x.RoomNumber,
-                Type = x.Type,
-                Capacity = x.Capacity,
-                Status = x.Status,
-                ImageUrl = x.ImageUrl,
-                Price = x.Price,
-                Description = x.Description
-            }),
-            Types = data.GroupBy(x => x.Type).Select(x => new RoomGroupResponseDto
-            {
-                Type = x.Key,
-                TotalFree = x.Count(x => x.Status == RoomStatus.Libre),
-                TotalTaken = x.Count(x => x.Status == RoomStatus.Reservada),
-                TotalMaintenance = x.Count(x => x.Status == RoomStatus.Mantenimiento),
-                Total = x.Count()
-            }),
+            //Data = data.Select(x => new RoomDto()
+            //{
+            //    RoomId = x.RoomId,
+            //    RoomNumber = x.RoomNumber,
+            //    Type = x.Type,
+            //    Capacity = x.Capacity,
+            //    Status = x.Status,
+            //    ImageUrl = x.ImageUrl,
+            //    Price = x.Price,
+            //    Description = x.Description
+            //}),
+            //Types = groups.Select(x => new RoomGroupResponseDto
+            //{
+            //    Type = x.Key,
+            //    TotalFree = x.Count(x => x.Status == RoomStatus.Libre),
+            //    TotalTaken = x.Count(x => x.Status == RoomStatus.Reservada),
+            //    TotalMaintenance = x.Count(x => x.Status == RoomStatus.Mantenimiento),
+            //    Total = x.Count()
+            //}),
 
-            TotalRooms = data.Count(),
-            TotalTaken = data.Count(x => x.Status == RoomStatus.Reservada),
-            TotalFree = data.Count(x => x.Status == RoomStatus.Libre),
-            TotalMaintenance = data.Count(x => x.Status == RoomStatus.Mantenimiento),
-            Sencilla = data.Count(x => x.Type == RoomType.Sencilla),
-            Doble = data.Count(x => x.Type == RoomType.Doble),
-            Familiar = data.Count(x => x.Type == RoomType.Familiar),
-            Triple = data.Count(x => x.Type == RoomType.Triple),
-            King = data.Count(x => x.Type == RoomType.King),
-            Presidencial = data.Count(x => x.Type == RoomType.Presidencial),
-            Mini = data.Count(x => x.Type == RoomType.Mini),
+            //TotalRooms = data.Count(),
+            //TotalTaken = data.Count(x => x.Status == RoomStatus.Reservada),
+            //TotalFree = data.Count(x => x.Status == RoomStatus.Libre),
+            //TotalMaintenance = data.Count(x => x.Status == RoomStatus.Mantenimiento),
+            //Sencilla = data.Count(x => x.Type == RoomType.Sencilla),
+            //Doble = data.Count(x => x.Type == RoomType.Doble),
+            //Familiar = data.Count(x => x.Type == RoomType.Familiar),
+            //Triple = data.Count(x => x.Type == RoomType.Triple),
+            //King = data.Count(x => x.Type == RoomType.King),
+            //Presidencial = data.Count(x => x.Type == RoomType.Presidencial),
+            //Mini = data.Count(x => x.Type == RoomType.Mini),
         };
 
-        return dto;
+       
+
+        return d;
     }
 
     public List<AvailableRoomsDto> GetAvailableRooms(RoomTypes? type)
