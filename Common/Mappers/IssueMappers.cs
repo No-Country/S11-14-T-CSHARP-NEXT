e4d81aad7d3a25779bb1bad381faa0ea3447874b
DnTo <danto@hotmail.com>
2023-10-29 23:53:50 -0500

    feat(issues): add search

---
+++ Common/Mappers/IssueMappers.cs
@@ -0,0 +1,31 @@
+﻿using S11.Common.Dto;
+using S11.Data.Models;
+
+namespace HotelWiz.Common.Mappers
+{
+    public static class IssueMappers
+    {
+        public static IList<IssueDto> MapperIssueToDto(this IEnumerable<Issue> issues)
+        {
+            var dtos = new List<IssueDto>();
+            foreach (var item in issues)
+            {
+                dtos.Add(
+                    new IssueDto
+                    {
+                        DateIssue = item.DateIssue,
+                        RoomId = item.RoomId,
+                        Area = item.Area,
+                        Category = item.Category,
+                        Description = item.Description,
+                        GuestId = item.GuestId,
+                        IssueId = item.IssueId,
+                        ReportedBy = item.ReportedBy,
+                        Status = item.Status,
+                        Title = item.Title
+                    });
+            }
+            return dtos;
+        }
+    }
+}
